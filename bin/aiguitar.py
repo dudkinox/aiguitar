2d7676fd92f237b087d79504dc78b5cc28a0d486
vendor505| <guitar@odds.team>
2024-02-21 14:53:18 +0700

    Update aiguitar.py

--- bin/aiguitar.py
+++ bin/aiguitar.py
@@ -1,15 +1,24 @@
 import requests
 import json
 
+# Black: 30
+# Red: 31
+# Green: 32
+# Yellow: 33
+# Blue: 34
+# Magenta: 35
+# Cyan: 36
+# White: 37
+
 while(True):
-    print("AIGuitar : ", end="")
+    print("\033[31mAIGuitar : \033[0m", end="")
     question = str(input())
-    print("Downloading...")
+    print("\033[32mDownloading...\033[0m")
     if question == "/q":
         break
     if question == "/help":
-        print(" /q => exit ")
-        print(" /help => all command ")
+        print("\033[35m /q => exit \033[0m")
+        print("\033[35m /help => all command \033[0m")
         continue
     url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=AIzaSyDOfLJFYbEX_6XLhAR5fN462X2FYPiU-oo"
     payload = json.dumps({
@@ -29,6 +38,6 @@ while(True):
     response = requests.request("POST", url, headers=headers, data=payload)
     responseText = response.text
     data = json.loads(responseText)
-    print(data['candidates'][0]['content']['parts'][0]['text'])
+    print("\033[34m" + data['candidates'][0]['content']['parts'][0]['text'] + "\033[0m")
     
 print("bye.")
