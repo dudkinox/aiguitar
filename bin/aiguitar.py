87edccc981b3563971904bc30bf0a78166db73c4
vendor505| <guitar@odds.team>
2024-02-21 13:21:06 +0700

    Update aiguitar.py

--- bin/aiguitar.py
+++ bin/aiguitar.py
@@ -1,7 +1,34 @@
+import requests
+import json
+
 while(True):
     print("AIGuitar : ", end="")
     question = str(input())
-    print(question)
+    print("Downloading...")
     if question == "/q":
         break
+    if question == "/help":
+        print(" /q => exit ")
+        print(" /help => all command ")
+        continue
+    url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=AIzaSyDOfLJFYbEX_6XLhAR5fN462X2FYPiU-oo"
+    payload = json.dumps({
+    "contents": [
+        {
+        "parts": [
+            {
+            "text": question
+            }
+        ]
+        }
+    ]
+    })
+    headers = {
+    'Content-Type': 'application/json'
+    }
+    response = requests.request("POST", url, headers=headers, data=payload)
+    responseText = response.text
+    data = json.loads(responseText)
+    print(data['candidates'][0]['content']['parts'][0]['text'])
+    
 print("bye.")
